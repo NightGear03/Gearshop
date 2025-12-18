a0876eef9f23c0e81bec40bd269b0ab22187295c
NightGear03 <roihanfebrilian303@gmail.com>
2025-12-18 14:13:16 +0700

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "Gearshop",
+  description: "Katalog item & harga"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0, fontFamily: "sans-serif" }}>
+        {children}
+      </body>
+    </html>
+  );
+}
