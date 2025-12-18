feb0f5707d74515a59b865d815ce202dff36be9e
NightGear03 <roihanfebrilian303@gmail.com>
2025-12-18 14:11:04 +0700

    page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+export default function Page() {
+  return (
+    <main style={{ padding: 20, fontFamily: "sans-serif" }}>
+      <h1>Gearshop</h1>
+      <p>Katalog item & harga</p>
+      <a href="https://wa.me/6283101456267">
+        Chat Admin GearShop
+      </a>
+    </main>
+  );
+}
